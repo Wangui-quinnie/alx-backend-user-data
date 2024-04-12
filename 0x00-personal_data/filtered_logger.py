e5776f7446fa33b1d86a6cfe41881b076d6756b4
Wangui-quinnie <w.wangui@ymail.com>
2024-04-12 08:05:18 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Module for filtering log data
 """
 
+import logging
 import re
 from typing import List
 
@@ -32,3 +33,29 @@ def filter_datum(
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format method to filter values in incoming log records
+        """
+        message = record.msg
+        for field in self.fields:
+            message = re.sub(
+                rf'{field}=.+?{self.SEPARATOR}',
+                f'{field}={self.REDACTION} {self.SEPARATOR}',
+                message
+            )
+        record.msg = message
+        return super().format(record)
