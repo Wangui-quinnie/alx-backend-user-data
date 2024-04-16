bba6ccd6e9db50834ac872d3890a0b814668dd1b
Wangui-quinnie <w.wangui@ymail.com>
2024-04-16 22:30:44 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Extract authorization header from the request."""
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None):
         """Get the current user from the request."""
