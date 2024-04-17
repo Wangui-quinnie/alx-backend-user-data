78c9edacb9a1938a4cbe077e783419edd402845e
Wangui-quinnie <w.wangui@ymail.com>
2024-04-17 04:30:26 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,18 @@ class BasicAuth(Auth):
             return None
         base64_part = authorization_header[6:]
         return base64_part
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str
+    ) -> str:
+        """Decode the Base64 string."""
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except Exception:
+            return None
