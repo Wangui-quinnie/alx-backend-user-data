717b2a253ebc2bbd17e1dac5bad61270f9eba73b
Wangui-quinnie <w.wangui@ymail.com>
2024-04-19 02:09:39 +0000

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Session based authentication"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class representing session-based authentication."""
+    pass
