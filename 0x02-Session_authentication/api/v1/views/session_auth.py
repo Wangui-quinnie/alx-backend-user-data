a8a4166a6aa796c67f7498f785c72632ee71a9bd
Wangui-quinnie <w.wangui@ymail.com>
2024-04-19 02:49:04 +0000

    New view for Session Authentication

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Session authentication route."""
+
+
+from flask import Flask, jsonify, request
+from api.v1.views import app_views
+from models.user import User
+from api.v1.app import auth
+
+
+@app_views.route('/auth_session/login', methods=['POST', 'GET'],
+                 strict_slashes=False)
+def session_login():
+    """Handle user login using session authentication."""
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email:
+            return jsonify({"error": "email missing"}), 400
+        if not password:
+            return jsonify({"error": "password missing"}), 400
+
+        user = User.search({'email': email})
+        if not user:
+            return jsonify({"error": "no user found for this email"}), 404
+
+        if not user[0].is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
+        session_id = auth.create_session(user[0].id)
+        response = jsonify(user[0].to_json())
+        response.set_cookie(auth.session_name, session_id)
+        return response, 200
+
+    return jsonify({"error": "Method Not Allowed"}), 405
